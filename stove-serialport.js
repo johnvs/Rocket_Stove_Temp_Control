43ddcf502ff1a380772c9aed353c3809bbb4cadd
john van strien <git_freelance@johnvanstrien.com>
2017-04-10 20:48:11 -0700

    Deleted code inherited from stove-controller module that doesn’t apply to the serial port. Deleted the on and emit methods from the module.exports object. Added the addOnChangeHandlers, sendStr and init methods, and the isConnected variable to the module.exports object. Deleted the emitters object. In the serialport on data callback, added a second try/catch block

--- stove-serialport.js
+++ stove-serialport.js
@@ -1,120 +1,31 @@
 "use strict";
 
 const SerialPort = require('serialport');
-// const controlModes = require('./control-modes');
 
 let serialPort;
-let dataRecord = {};
 
-let callbacks = {};
-// Looks like:
-// callbacks = {
-//                "update" : updateCallback,
-//                "controllerConnected" : ccCallback
-//             }
+let isConnected = false;
+
+let onChangeHandlers;
 
 module.exports = {
-    on : function(eventName, callback) {
-            console.log("stove-controller:on " + eventName);
-            callbacks[eventName] = callback;
-          },
 
-    emit : function(eventName, data) {
-              console.log("stove-controller:emit " + eventName);
+  addOnChangeHandlers : function(handlers) {
+    // Handlers for events:
+    //   controllerConnected
+    //    dataRcvd
+    onChangeHandlers = handlers;
+  },
 
-              if (typeof emitters[eventName] === "function") {
-                emitters[eventName](data);
-              } else {
-                console.log("event " + eventName + " is not a function");
-              }
+  sendStr: function(theString) {
+             if (serialPort) {
+               console.log('stove-serialport:sendStr: ' + theString);
+               serialPort.write(theString);
+             }
            },
 
-    init : initSerialPort
-};
-
-  a : function(data) {
-        // a  Pot thermocouple faults
-        if ((typeof data === "number") && (data >= 0) && (data < 4)) {
-          // $('#potTCFaults').text(tcFaults[data]);
-          callbacks.update('potTCFaults', tcFaults[data]);
-        }
-      },
-
-const emitters = {
-
-  "homeMotorBtnClicked" : function() {
-        if (serialPort) {
-          console.log('Home Motor button was clicked.');
-      		serialPort.write('h\r');
-        // } else {
-        //   const warningMsg = 'Tried to home damper motor while not connected to stove controller';
-        //   console.log(warningMsg);
-        //   // Signal the client that it is not connected to a stove controller
-        //   socketServer.emit('warning', warningMsg);
-        }
-    	},
-
-  "potTempDesiredChanged" : function(data) {
-        if (serialPort) {
-          console.log('Desired pot temp new value = ' + data);
-      		serialPort.write('m ' + data + '\r');
-        // } else {
-        //   console.log('Tried to change desired pot temp while not connected to stove controller');
-        }
-    	},
-
-  "damperCntlModeChanged" : function(data) {
-        if (serialPort) {
-          if (controlModes.damper.hasOwnProperty(data)) {
-            console.log('Damper Control Mode new value ' + data + ' = ' + controlModes.damper[data]);
-        		serialPort.write('a ' + controlModes.damper[data] + '\r');
-          } else {
-            console.log('Bad damper control mode value ' + data);
-          }
-        // } else {
-        //   console.log('Tried to change desired pot temp while not connected to stove controller');
-        }
-    	},
-
-  "damperAngleChanged" : function(data) {
-        if (serialPort) {
-          console.log('Damper angle new value = ' + data);
-      		serialPort.write('n ' + data + '\r');
-        // } else {
-        //   console.log('Tried to change damper angle while not connected to stove controller');
-        }
-    	},
-
-  "blowerCntlModeChanged" : function(data) {
-        if (serialPort) {
-          if (controlModes.blower.hasOwnProperty(data)) {
-            console.log('Blower Control Mode new value ' + data + ' = ' + controlModes.blower[data]);
-        		serialPort.write('b ' + controlModes.blower[data] + '\r');
-          } else {
-            console.log('Bad blower control mode value ' + data);
-          }
-        // } else {
-        //   console.log('Tried to change desired pot temp while not connected to stove controller');
-        }
-    	},
-
-  "blowerSpeedChanged" : function(data) {
-        if (serialPort) {
-          console.log('Blower speed new value = ' + data);
-      		serialPort.write('f ' + data + '\r');
-        // } else {
-        //   console.log('Tried to change blower speed while not connected to stove controller');
-        }
-    	},
-
-  "dataUpdateRateChanged" : function(data) {
-        if (serialPort) {
-          console.log('Data update rate new value = ' + data);
-      		serialPort.write('q ' + data + '\r');
-        // } else {
-        //   console.log('Tried to change data update rate while not connected to stove controller');
-        }
-    	}
+  init : initSerialPort,
+  isConnected : isConnected
 };
 
 const serialPortConfig = {
@@ -127,8 +38,7 @@ const serialPortConfig = {
 };
 
 function initSerialPort() {
-  console.log('Initializing serial ports.');
-
+  console.log('Initializing serial port.');
   SerialPort.list(checkPortAvailability);
 }
 
@@ -153,8 +63,9 @@ function checkPortAvailability(err, ports) {
     if (!serialPort) {
       console.log("Failed to find an open a serial port.");
 
-      // Let client know that it is NOT connected to a stove controller.
-      callbacks.controllerConnected(false);
+      // Let interface controller know that it is NOT connected to a stove controller (Teensy).
+      onChangeHandlers.controllerConnected(false);
+
     }
   }
 }
@@ -175,8 +86,8 @@ function portOpenCallback(err) {
 
     // After confirming that the connected serial device is a stove controller,
     // let client know that it is connected.
-    callbacks.controllerConnected(true);
-    isStoveConnected = true;
+    onChangeHandlers.controllerConnected(true);
+    isConnected = true;
   }
 }
 
@@ -188,25 +99,27 @@ function defineSerialPortEventHandlers() {
     // Processes incoming serial data received as string
     serialPort.on('data', function(data) {
 
-      let dataObj;
-      try {
         // console.log("serial data = " + data);
         // console.log("typeof data = " + typeof data);
+        let dataObj;
+        try {
+          dataObj = JSON.parse(data);
+          console.log('New serial data:');
+          console.log(dataObj);
 
-        dataObj = JSON.parse(data);
-        console.log("New serial data:");
-        console.log(dataObj);
+          try {
+            // Send the interface controller the new serial data
+            onChangeHandlers.dataRcvd(dataObj);
+          }
+          catch (err) {
+            console.log('Error processing received data: ' + err);
+          }
+        }
+        catch (err) {
+          console.log('Error parsing received data: ' + err);
+          console.log('Non-JSON serial data received: \n' + data);
+        }
 
-        // Send the client that new serial data
-        callbacks.update(dataObj);
-        // for (let cb in callbacks) {
-        //   console.log("callback " + cb + " is " + callbacks[cb]);
-        // }
-      }
-      catch (err) {
-        // console.log('Serial port error = ' + err);
-        console.log('Non-JSON serial data received: \n' + data);
-      }
     });
   });
 }
